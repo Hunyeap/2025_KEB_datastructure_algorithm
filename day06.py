b71430139d2cf6cac0e587bad07c51f5f73cd43f
hunyeap <choihunyeap@gmail.com>
2025-02-17 17:18:43 +0900

    v1.3 day06

--- day06.py
+++ day06.py
@@ -1,13 +1,18 @@
+import csv
 import random
 
-students = []
 try:
-    file = input("File name: ")
-    fp = open(file, 'r')
-    readme_list = fp.readlines()
-    rls = readme_list[0].split()
-    print(readme_list)
-    print(rls)
-    fp.close()
+    with open("KSEB_students.csv", 'r', encoding="euc-kr") as fp:
+        student_list = fp.readlines()
+        student_list.remove("이상혁\n")
+        student_list.remove("조윤하\n")
+        student_list.remove("김철중\n")
+        student_list.remove("김현민\n")
+        student_list.remove("김찬빈\n")
+        for i in range(3):
+            random_pick = random.choice(student_list)
+            print(random_pick, end = '')
+            student_list.remove(random_pick)
+        #print(student_list[random.randint(0, len(student_list) - 1)])
 except FileNotFoundError as err:
-    print(f"{file} is not exist. {err}")
+    print(err)
