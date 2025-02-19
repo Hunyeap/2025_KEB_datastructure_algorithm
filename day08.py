bb8d84849774c1dce461f1e5980edea12d82dfe8
hunyeap <choihunyeap@gmail.com>
2025-02-19 13:41:01 +0900

    3.4

--- day08.py
+++ day08.py
@@ -1,68 +1,47 @@
-def pre_order(node):
-    if node is None:
-        return
-    print(node.data, end = '->')
-    pre_order(node.left)
-    pre_order(node.right)
-
-def in_order(node):
-    if node is None:
-        return
-    in_order(node.left)
-    print(node.data, end = '->')
-    in_order(node.right)
-
-def post_order(node):
-    if node is None:
-        return
-    post_order(node.left)
-    post_order(node.right)
-    print(node.data, end = '->')
-
-
 class TreeNode:
-	def __init__(self):
-		self.left = None
-		self.data = None
-		self.right = None
+    def __init__(self):
+        self.left = None
+        self.data = None
+        self.right = None
 
+if __name__ == "__main__":
+    groups = ['블랙핑크', '레드벨벳', '마마무', '에이핑크', '걸스데이', '트와이스']
+    # groups = [10, 15, 4, 3, 27, 40]
+    root = None
 
-node1 = TreeNode()
-node1.data = 'hs'
+    node = TreeNode()
+    node.data = groups[0]
+    root = node
+    for group in groups[1:]:
+        node = TreeNode()
+        node.data = group
+        current = root
+        while True:
+            if group < current.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right
+    print("BST complete")
+    find_group = input()
 
-node2 = TreeNode()
-node2.data = 'sl'
-node1.left = node2
-
-node3 = TreeNode()
-node3.data = 'mb'
-node1.right = node3
-
-node4 = TreeNode()
-node4.data = 'hw'
-node2.left = node4
-
-node5 = TreeNode()
-node5.data = 'zz'
-node2.right = node5
-
-node6 = TreeNode()
-node6.data = 'sm'
-node3.left = node6
-
-node7 = TreeNode()
-node7.data = 'dh'
-node4.right = node7
-
-node8 = TreeNode()
-node8.data = 'sn'
-node6.right = node8
-
-# print(node6.data)
-# print(node1.right.left.data)
-
-print(f"{pre_order(node1)}\n")
-print(f"{in_order(node1)}\n")
-print(f"{post_order(node1)}\n")
-
-pre_order(node1)
+    current = root
+    while True:
+        if find_group == current.data:
+            print(f"{find_group}을(를) 찾았습니다.")
+            break
+        elif find_group < current.data:
+            if current.left is None:
+                print(f"{find_group}이(가) 존재하지 않습니다.")
+                break
+            current = current.left
+        else:
+            if current.right is None:
+                print(f"{find_group}이(가) 존재하지 않습니다.")
+                break
+            current = current.right
